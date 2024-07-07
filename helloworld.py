221825d1b848c40522ca8d69c2936334471e8e01
Joulu "Tok" Heikkinen <bobaviet73@gmail.com>
2024-07-07 15:00:55 -0700

    I wrote 3 sentence to start my journey in GitHub and Python

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+#Printing out the Basic Hello World to start learning GitHub
+
+print("Hello World!")
+
+print("My Goal is to learn Python, Electronics, and GitHub")
+      
+print("I hope to learn Machine Learning, or AI")
